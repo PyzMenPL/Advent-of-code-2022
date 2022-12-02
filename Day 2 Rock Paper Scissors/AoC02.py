0a400b952991756fefb30dd8f51f1c34d0dab118
Niskative <niskative@proton.me>
2022-12-02 19:27:45 +0100

    Removed unnecessary print()

--- Day 2 Rock Paper Scissors/AoC02.py
+++ Day 2 Rock Paper Scissors/AoC02.py
@@ -13,12 +13,10 @@ with open('02input.txt', 'r') as oFile:
 
         # Win
         if line == 'A Y\n' or line == 'B Z\n' or line == 'C X\n':
-            print('Win')
             suma += 6
         # Draw
         elif line == 'A X\n' or line == 'B Y\n' or line == 'C Z\n':
             suma += 3
-            print("Draw")
         # You don't have to check if you are losing,
         # because it doesn't change the output
 
