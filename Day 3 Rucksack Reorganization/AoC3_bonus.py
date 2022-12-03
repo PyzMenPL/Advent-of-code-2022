588afb310b309df14fed3196d67656fb89f98e21
Niskative <niskative@proton.me>
2022-12-03 13:52:16 +0100

    English comments in AoC3_bonus.py

--- Day 3 Rucksack Reorganization/AoC3_bonus.py
+++ Day 3 Rucksack Reorganization/AoC3_bonus.py
@@ -1,34 +1,34 @@
 with open('03input.txt', 'r') as oFile:
-    # Lista elementów, które pojawiają się w obu listach
+    # List of elements, in one group
     both = []
 
     # Lista elementów, w jednej grupie
     group = []
 
     for line in oFile:
-        # Dodajemy każdą linijkę
+        # We add each line
         group.append(set(line[:-1]))
 
-        # Jeżeli mamy 3 elfy w grupie
+        # For each character in the smallest set
         if len(group) == 3:
             # Dla każdego znaku w najmniejszym zbiorze
             for letter in min(group):
-                # Sprawdź, czy występuje w reszcie
+                # Check if it occurs in the rest
                 if letter in group[group.index(min(group)) - 1] and letter in group[group.index(min(group)) - 2]:
-                    # Przydziel do listy
+                    # Assign to a list
                     both.append(letter)
 
-            # Po znalezieniu grupy 3 elfów wyczyść listę group
+            # After finding a group of 3 elves, clear group
             group = []
 
-    # Bez żadnych obaw! Wygenerowałem to :D
+    # No worries! I generated this :D
     values = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12,
               'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23,
               'x': 24, 'y': 25, 'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34,
               'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45,
               'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
 
-    # Suma wyników
+    # Total results
     suma = 0
 
     for letter in both:
