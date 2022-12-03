2fc90d2850f0e8ee47ca0033bacb947ebac48eb4
Niskative <niskative@proton.me>
2022-12-03 15:02:29 +0100

    There are now no lists in between searching through lines and summing everything up (but now in AoC3.py)

--- Day 3 Rucksack Reorganization/AoC3.py
+++ Day 3 Rucksack Reorganization/AoC3.py
@@ -1,13 +1,6 @@
 with open('03input.txt', 'r') as oFile:
-    # List of items that appear in both lists
-    both = []
-
-    for line in oFile:
-        # Sorting characters into appropriate lists
-        for letter in set(line[:int(len(line) / 2)]):
-            if letter in set(line[int(len(line) / 2):-1]):
-                both.append(letter)
-                break
+    # Total results
+    suma = 0
 
     # No worries! I generated this :D
     values = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12,
@@ -16,10 +9,11 @@ with open('03input.txt', 'r') as oFile:
               'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45,
               'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
 
-    # Total results
-    suma = 0
-
-    for letter in both:
-        suma += values[letter]
+    for line in oFile:
+        # Sorting characters into appropriate lists
+        for letter in set(line[:int(len(line) / 2)]):
+            if letter in set(line[int(len(line) / 2):-1]):
+                suma += values[letter]
+                break
 
     print(suma)
