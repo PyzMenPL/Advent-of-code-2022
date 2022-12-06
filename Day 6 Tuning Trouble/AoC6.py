6fcc103f969a9ffdae281d691256027342c0e32e
Niskative <niskative@proton.me>
2022-12-06 19:18:27 +0100

    Shrunken the range by unnecessary letters

--- Day 6 Tuning Trouble/AoC6.py
+++ Day 6 Tuning Trouble/AoC6.py
@@ -1,7 +1,8 @@
 with open('input.txt', 'r') as oFile:
     for line in oFile:
-        for index in range(0, len(line) + 1):
+        # Starting from 4 because we don't check unnecessary 3 first letters
+        for index in range(4, len(line) + 1):
             # If there are 4 unique values before index
-            if len(set(line[index - 4 : index])) == 4:
+            if len(set(line[index - 4: index])) == 4:
                 print(index)
                 break
--- Day 6 Tuning Trouble/AoC6_bonus.py
+++ Day 6 Tuning Trouble/AoC6_bonus.py
@@ -1,6 +1,7 @@
 with open('input.txt', 'r') as oFile:
     for line in oFile:
-        for index in range(0, len(line) + 1):
+        # Starting from 4 because we don't check unnecessary 3 first letters
+        for index in range(14, len(line) + 1):
             # If there are 14 unique values before index
             if len(set(line[index - 14: index])) == 14:
                 print(index)
